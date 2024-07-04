521b1d899d67d90335ce5e3428faaaf119315d62
Artem0202 <artyomkov.19@gmail.com>
2024-07-04 09:19:22 +0300

    create

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+const PORT = 3000;
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: 'Not Found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+setupServer();
